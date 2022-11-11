6f8b8a82ad01f4c3cbe7e6000728d4a49e7ecbd5
Pakize Bozkurt <pakiozihak@gmail.com>
2022-11-11 23:02:06 +0000

    Added icon

--- script.js
+++ script.js
@@ -13,7 +13,12 @@ script.get("/", function (req, res) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const weatherDesc = weatherData.weather[0].description;
-      res.send("<h1>The temprature in London is " + temp + "degrees Celcius.</h1>")
+      const icon = weatherData.weather[0].icon
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+      res.write("<p>The weather is currently " + weatherDesc +"</p>");
+      res.write("<h1>The temprature in London is " + temp + "degrees Celcius.</h1>")
+      res.write("<img src=" + imageURL + "/>")
+      res.send()
     });
   });
 });
