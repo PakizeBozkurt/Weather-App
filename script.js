36e5c6053e3652123f6f350aa0bb6defef8dade9
Pakize Bozkurt <pakiozihak@gmail.com>
2022-11-11 22:45:54 +0000

    Changed api address

--- script.js
+++ script.js
@@ -1,53 +1,77 @@
-window.addEventListener("load", () => {
-  let long;
-  let lat;
-  let tempDescrp = document.querySelector(".temperature-description");
-  let tempDegree = document.querySelector(".temperature-degree");
-  let locationTimezone = document.querySelector(".location-timezone");
-  let tempratureSection = document.querySelector(".temperature");
-  const temperatureSpan = document.querySelector(".temperature span");
+const express = require("express");
+const https = require("https");
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
+const script = express();
 
-      const proxy = `https://cors-anywhere.herokuapp.com/`;
-      const api = `${proxy}https://api.darksky.net/forecast/1dda89e902ce89b77ed2412eac3026d7/${lat},${long}`;
+script.get("/", function(req, res){
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e72ca729af228beabd5d20e3b7749713&units=metric"
+  https.get(url, function(response){
+    console.log(response.statusCode);
 
-      fetch(api)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          const { temprature, summary, icon } = data.currently;
-          //Set DOM Elements from the API
-          tempDegree.textContent = temprature;
-          tempDescrp.textContent = summary;
-          locationTimezone.textContent = data.timezone;
-          //Formula for Celsius
-          let celcius = (temprature - 32) * (5 / 9);
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp
+      const weatherDesc = weatherData.weather[0].description
+    })
+  })
+  res.send("Server is up and runing.")
+})
 
-          //Set Icon
-          setIcons(icon, document.querySelector(".icon"));
-          // Change temprature to Celsius/Fahrenheit
-          tempratureSection.addEventListener("click", () => {
-            if (temperatureSpan.textContent === "F") {
-              temperatureSpan.textContent = "C";
-              tempDegree.textContent = Math.floor(celcius);
-            } else {
-              temperatureSpan.textContent = "F";
-              tempDegree.textContent = temprature;
-            }
-          });
-        });
-    });
-  }
-  function setIcons(icon, iconID) {
-    const skycons = new Skycons({ color: "white" });
-    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-    skycons.play();
-    return skycons.set(iconID, Skycons[currentIcon]);
-  }
-});
+
+
+script.listen(3000, function() {
+  console.log("Server is running on port 3000");
+})
+// window.addEventListener("load", () => {
+//   let long;
+//   let lat;
+//   let tempDescrp = document.querySelector(".temperature-description");
+//   let tempDegree = document.querySelector(".temperature-degree");
+//   let locationTimezone = document.querySelector(".location-timezone");
+//   let tempratureSection = document.querySelector(".temperature");
+//   const temperatureSpan = document.querySelector(".temperature span");
+
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition((position) => {
+//       console.log(position);
+//       long = position.coords.longitude;
+//       lat = position.coords.latitude;
+
+//       const proxy = `https://cors-anywhere.herokuapp.com/`;
+//       const api = `${proxy}https://api.darksky.net/forecast/1dda89e902ce89b77ed2412eac3026d7/${lat},${long}`;
+
+//       fetch(api)
+//         .then((response) => {
+//           return response.json();
+//         })
+//         .then((data) => {
+//           const { temprature, summary, icon } = data.currently;
+//           //Set DOM Elements from the API
+//           tempDegree.textContent = temprature;
+//           tempDescrp.textContent = summary;
+//           locationTimezone.textContent = data.timezone;
+//           //Formula for Celsius
+//           let celcius = (temprature - 32) * (5 / 9);
+
+//           //Set Icon
+//           setIcons(icon, document.querySelector(".icon"));
+//           // Change temprature to Celsius/Fahrenheit
+//           tempratureSection.addEventListener("click", () => {
+//             if (temperatureSpan.textContent === "F") {
+//               temperatureSpan.textContent = "C";
+//               tempDegree.textContent = Math.floor(celcius);
+//             } else {
+//               temperatureSpan.textContent = "F";
+//               tempDegree.textContent = temprature;
+//             }
+//           });
+//         });
+//     });
+//   }
+//   function setIcons(icon, iconID) {
+//     const skycons = new Skycons({ color: "white" });
+//     const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+//     skycons.play();
+//     return skycons.set(iconID, Skycons[currentIcon]);
+//   }
+// });
