3dfd3e1fb6177072826e765ca3a33f84736debdb
Pakize Bozkurt <pakiozihak@gmail.com>
2022-11-12 21:35:53 +0000

    Trying to do short way

--- script.js
+++ script.js
@@ -4,28 +4,36 @@ const https = require("https");
 const script = express();
 
 script.get("/", function (req, res) {
-
-  const query = "London";
-  const apiKey = "e72ca729af228beabd5d20e3b7749713";
-  const unit = "metric";
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey +"&units=" + unit;
-  https.get(url, function (response) {
-    console.log(response.statusCode);
-
-    response.on("data", function (data) {
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      const weatherDesc = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon
-      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-      res.write("<p>The weather is currently " + weatherDesc +"</p>");
-      res.write("<h1>The temprature in London is " + temp + "degrees Celcius.</h1>")
-      res.write("<img src=" + imageURL + "/>")
-      res.send()
-    });
-  });
+res.sendFile(__dirname + "/index.html");
+  
 });
+// const query = "London";
+// const apiKey = "e72ca729af228beabd5d20e3b7749713";
+// const unit = "metric";
+// const url =
+//   "https://api.openweathermap.org/data/2.5/weather?q=" +
+//   query +
+//   "&appid=" +
+//   apiKey +
+//   "&units=" +
+//   unit;
+// https.get(url, function (response) {
+//   console.log(response.statusCode);
+
+//   response.on("data", function (data) {
+//     const weatherData = JSON.parse(data);
+//     const temp = weatherData.main.temp;
+//     const weatherDesc = weatherData.weather[0].description;
+//     const icon = weatherData.weather[0].icon;
+//     const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+//     res.write("<p>The weather is currently " + weatherDesc + "</p>");
+//     res.write(
+//       "<h1>The temprature in London is " + temp + "degrees Celcius.</h1>"
+//     );
+//     res.write("<img src=" + imageURL + "/>");
+//     res.send();
+//   });
+// });
 
 script.listen(3000, function () {
   console.log("Server is running on port 3000");
